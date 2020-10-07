9696e720b749ca86f38de09a2a87f7b52f866dc4
Sara Gilbert <sara.gilbert@webropol.com>
2020-10-07 08:31:22 +0300

    yarn serve

---
+++ build/precache-manifest.f6ec5422be98e2403977e9dd257abe00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0210d556daefc5314a6d154b2c9fcd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a1033611f4ef2640e9",
+    "url": "/static/css/main.94b4a744.chunk.css"
+  },
+  {
+    "revision": "dac2f99c9e5fd8062bf1",
+    "url": "/static/js/2.9d068648.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9d068648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a1033611f4ef2640e9",
+    "url": "/static/js/main.0f358847.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
