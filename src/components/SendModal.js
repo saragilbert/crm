80609b51fb867da22b2251955174e711333784d8
Sara Gilbert <sara.gilbert@webropol.com>
2020-10-07 16:06:46 +0300

    adds modal

--- src/components/SendModal.js
+++ src/components/SendModal.js
@@ -1,31 +1,57 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Button, Modal, ListGroup, Col, Row, Table} from 'react-bootstrap'
-import { Bearer } from './data/DB'
+import { customerDB } from './data/DB'
+import BootstrapTable from 'react-bootstrap-table-next'
 
 function SendModal() {
    const [show, setShow] = React.useState(false);
-
    const handleClose = () => setShow(false);
    const handleShow = () => setShow(true);
+   const cols = [{
+      dataField: 'firstName',
+      text: 'Nimi'
+   },
+   {
+      dataField: 'email',
+      text:'Sähköjen posti'
+   },{
+      dataField: 'phone',
+      text: 'NMT/GSM'
+   },
+   {
+      dataField: 'id',
+      text: 'id'
+   }];
 
+   const selectRow = {
+      mode: 'checkbox',
+      clickToSelect: true
+   }
    return (
      <div>
        <Button variant="primary" onClick={handleShow}>
          Lähetä kysely
        </Button>
 
-       <Modal show={show} onHide={handleClose}>
+       <Modal size="lg" show={show} onHide={handleClose}>
          <Modal.Header closeButton>
            <Modal.Title>Lähetyksen asetukset</Modal.Title>
          </Modal.Header>
-         <Modal.Body>MEEP</Modal.Body>
+         <Modal.Body>
+            <BootstrapTable
+            keyField="id"
+            data={customerDB}
+            columns={cols}
+            selectRow={selectRow}
+            />
+         </Modal.Body>
          <Modal.Footer>
            <Button variant="secondary" onClick={handleClose}>
-             Close
+             Sule tää roska!
            </Button>
            <Button variant="primary" onClick={handleClose}>
-             Save Changes
+             Lähetä!
            </Button>
          </Modal.Footer>
        </Modal>
